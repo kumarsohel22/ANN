999a18f53d44406b386204cfd063336a7dd765fc
kumarsohel22 <51870508+kumarsohel22@users.noreply.github.com>
2025-11-05 19:54:57 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
+import pickle
